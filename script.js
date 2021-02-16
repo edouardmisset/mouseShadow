6474cff122f6521bf89e8630552f02bf52b7a841
Edouard Misset <edouardmisset@gmail.com>
2021-02-16 14:05:06 +0100

    some style changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const style = document.querySelector("h1").style;
 const hero = document.querySelector(".hero");
 const { offsetWidth: width, offsetHeight: height } = hero;
-const maxShadowOffset = 30;
+const maxShadowOffset = 20;
 
 // Adding event listener
 document.querySelector("body").addEventListener("mousemove", updateShadow);
@@ -14,5 +14,9 @@ function updateShadow(e) {
   x = maxShadowOffset * (1 / 2 - x / width);
   y = maxShadowOffset * (1 / 2 - y / width);
 
-  style.textShadow = `${x}px ${y}px 10px hsl(40, 100%, 50%, 80%)`;
+  style.textShadow = `
+  ${-x}px ${-y}px 15px hsla(0, 100%, 50%, 0.9),
+  ${-x}px ${y}px 15px hsla(30, 100%, 50%, 0.9),
+  ${x}px ${-y}px 15px hsla(60, 100%, 50%, 0.9)
+  `;
 }
